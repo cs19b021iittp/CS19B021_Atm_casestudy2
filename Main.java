91fb43bd07b78a19fe6506f7d2f37e57510d3cd7
harshithakapa1 <70951042+harshithakapa1@users.noreply.github.com>
2020-11-11 00:37:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+
+// NOTE: The customer needs to interact with the ATM via console
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Atm atm = new Atm(1);
+        atm.startAtm();
+    }
+
+}
